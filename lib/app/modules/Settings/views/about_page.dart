9d890aa0d4f006f11748eb3d30851402e42e3114
Dodi Aditya <dodiaridharma@gmail.com>
2024-12-25 21:08:47 +0800

    feat: slicing about page

--- lib/app/modules/Settings/views/about_page.dart
+++ lib/app/modules/Settings/views/about_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie_haven/app/shared/views/widget/section_list.dart';
 
 class AboutPage extends StatelessWidget {
   const AboutPage({super.key});
@@ -7,10 +8,166 @@ class AboutPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("About developer"),
+        title: const Text("About us"),
       ),
-      body: const Placeholder(
-        child: Text("About"),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(24),
+            child: Column(
+              children: [
+                SectionList(
+                  title: "Developer profile",
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        width: double.infinity,
+                        child: Column(
+                          children: [
+                            Container(
+                              width: 60,
+                              height: 60,
+                              decoration: const BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: DecorationImage(
+                                  image: AssetImage("assets/images/dodi.png"),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            const Text(
+                              "Dodi Aditya",
+                              style: TextStyle(
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Material(
+                        borderRadius: BorderRadius.circular(12),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 18,
+                            vertical: 24,
+                          ),
+                          child: Column(
+                            children: [
+                              _profileItem(
+                                label: "Name",
+                                value: "I Made Dodi Aditya Ari Dharma",
+                                context: context,
+                              ),
+                              _profileItem(
+                                label: "Profession",
+                                value: "Frontend Developer",
+                                context: context,
+                              ),
+                              _profileItem(
+                                label: "Email",
+                                value: "dodiaridharma@gmail.com",
+                                context: context,
+                              ),
+                              _profileItem(
+                                label: "Github",
+                                value: "DodyDharma440",
+                                context: context,
+                              ),
+                              _profileItem(
+                                label: "LinkedIn",
+                                value: "dodi-aditya-237154206",
+                                context: context,
+                                isLast: true,
+                              ),
+                            ],
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 50),
+                SectionList(
+                  title: "About this app",
+                  child: Column(
+                    children: [
+                      const Text(
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus convallis efficitur euismod. Etiam vestibulum massa eget tellus tincidunt, eu aliquet nulla fermentum. Sed fermentum nulla eget arcu facilisis commodo at ut neque. Mauris eget cursus ligula. Morbi id velit eu neque mollis pharetra. Nunc metus metus, ultrices vel eros a, hendrerit maximus purus.",
+                      ),
+                      const SizedBox(height: 32),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            "Movie",
+                            style: TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            "Haven",
+                            style: TextStyle(
+                              color: Theme.of(context).primaryColor,
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          )
+                        ],
+                      ),
+                      const Text("v 1.0.0"),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _profileItem({
+    required String label,
+    required String value,
+    required BuildContext context,
+    bool isLast = false,
+  }) {
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w600,
+              color: Theme.of(context).primaryColor,
+            ),
+          ),
+          const SizedBox(height: 2),
+          Text(
+            value,
+            style: const TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          !isLast
+              ? const Padding(
+                  padding: EdgeInsets.only(
+                    top: 4,
+                    bottom: 6,
+                  ),
+                  child: Divider(
+                    color: Colors.white12,
+                  ),
+                )
+              : const SizedBox(),
+        ],
       ),
     );
   }
--- lib/app/modules/Settings/views/account_page.dart
+++ lib/app/modules/Settings/views/account_page.dart
@@ -19,157 +19,159 @@ class AccountPage extends StatelessWidget {
         title: const Text("Account settings"),
       ),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(24),
-          child: Column(
-            children: [
-              FormBuilder(
-                key: _controller.accountFormKey,
-                child: SectionList(
-                  title: "Account information",
-                  child: Column(
-                    children: [
-                      const SizedBox(height: 6),
-                      TextInput(
-                        name: "name",
-                        label: "Name",
-                        validator: FormBuilderValidators.compose([
-                          FormBuilderValidators.required(),
-                        ]),
-                      ),
-                      const SizedBox(height: 16),
-                      TextInput(
-                        name: "email",
-                        label: "Email",
-                        validator: FormBuilderValidators.compose([
-                          FormBuilderValidators.required(),
-                          FormBuilderValidators.email()
-                        ]),
-                      ),
-                      const SizedBox(height: 20),
-                      Obx(
-                        () => Button(
-                          onPressed: () {
-                            var isValid = _controller
-                                    .accountFormKey.currentState
-                                    ?.validate() ??
-                                false;
-                            if (isValid) {
-                              var name = _controller.accountFormKey.currentState
-                                      ?.instantValue['name'] ??
-                                  "";
-                              var email = _controller.accountFormKey
-                                      .currentState?.instantValue['email'] ??
-                                  "";
-                              _controller.updateProfile(name, email);
-                            }
-                          },
-                          text: "Update profile",
-                          isLoading: _controller.isLoadingProfile.value,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
-              const SizedBox(height: 24),
-              const Divider(color: Colors.white30),
-              const SizedBox(height: 24),
-              FormBuilder(
-                key: _controller.passwordFormKey,
-                child: GetBuilder<AccountController>(
-                  builder: (controller) => SectionList(
-                    title: "Change password",
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(24),
+            child: Column(
+              children: [
+                FormBuilder(
+                  key: _controller.accountFormKey,
+                  child: SectionList(
+                    title: "Account information",
                     child: Column(
-                      children: controller.showUpdatePassword.value
-                          ? [
-                              const SizedBox(height: 6),
-                              TextInput(
-                                key: const Key("password"),
-                                name: "password",
-                                label: "Password",
-                                isPassword: true,
-                                validator: FormBuilderValidators.compose([
-                                  FormBuilderValidators.required(),
-                                  FormBuilderValidators.password()
-                                ]),
-                              ),
-                              const SizedBox(height: 16),
-                              TextInput(
-                                name: "confirmPassword",
-                                label: "Confirm Password",
-                                isPassword: true,
-                                errorText: controller.accountFormKey
-                                    .currentState?.errors['confirmPassword'],
-                                validator: FormBuilderValidators.compose(
-                                  [
-                                    FormBuilderValidators.required(),
-                                    FormBuilderValidators.equal(
-                                      controller.accountFormKey.currentState
-                                              ?.fields['password']?.value ??
-                                          "",
-                                      errorText:
-                                          "Confirm password is not equal to password",
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              const SizedBox(height: 20),
-                              Button(
-                                onPressed: () {
-                                  var value = _controller
-                                          .passwordFormKey
-                                          .currentState
-                                          ?.instantValue['password'] ??
-                                      "";
-
-                                  _controller.updatePassword(
-                                    value,
-                                  );
-                                },
-                                text: "Update password",
-                                isLoading: controller.isLoadingPassword.value,
-                              )
-                            ]
-                          : [
-                              const SizedBox(height: 6),
-                              TextInput(
-                                key: const Key("currentPassword"),
-                                name: "currentPassword",
-                                label: "Current password",
-                                validator: FormBuilderValidators.compose([
-                                  FormBuilderValidators.required(),
-                                ]),
-                                isPassword: true,
-                              ),
-                              const SizedBox(height: 20),
-                              Button(
-                                onPressed: () {
-                                  var isValid = _controller
-                                          .passwordFormKey.currentState
-                                          ?.validate() ??
-                                      false;
-                                  var value = _controller
-                                          .passwordFormKey
-                                          .currentState
-                                          ?.instantValue['currentPassword'] ??
-                                      "";
-
-                                  if (isValid) {
-                                    _controller.checkPassword(
-                                      value,
-                                    );
-                                  }
-                                },
-                                text: "Check password",
-                                isLoading: controller.isLoadingPassword.value,
-                              ),
-                            ],
+                      children: [
+                        const SizedBox(height: 6),
+                        TextInput(
+                          name: "name",
+                          label: "Name",
+                          validator: FormBuilderValidators.compose([
+                            FormBuilderValidators.required(),
+                          ]),
+                        ),
+                        const SizedBox(height: 16),
+                        TextInput(
+                          name: "email",
+                          label: "Email",
+                          validator: FormBuilderValidators.compose([
+                            FormBuilderValidators.required(),
+                            FormBuilderValidators.email()
+                          ]),
+                        ),
+                        const SizedBox(height: 20),
+                        Obx(
+                          () => Button(
+                            onPressed: () {
+                              var isValid = _controller
+                                      .accountFormKey.currentState
+                                      ?.validate() ??
+                                  false;
+                              if (isValid) {
+                                var name = _controller.accountFormKey
+                                        .currentState?.instantValue['name'] ??
+                                    "";
+                                var email = _controller.accountFormKey
+                                        .currentState?.instantValue['email'] ??
+                                    "";
+                                _controller.updateProfile(name, email);
+                              }
+                            },
+                            text: "Update profile",
+                            isLoading: _controller.isLoadingProfile.value,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
-              ),
-            ],
+                const SizedBox(height: 24),
+                const Divider(color: Colors.white30),
+                const SizedBox(height: 24),
+                FormBuilder(
+                  key: _controller.passwordFormKey,
+                  child: GetBuilder<AccountController>(
+                    builder: (controller) => SectionList(
+                      title: "Change password",
+                      child: Column(
+                        children: controller.showUpdatePassword.value
+                            ? [
+                                const SizedBox(height: 6),
+                                TextInput(
+                                  key: const Key("password"),
+                                  name: "password",
+                                  label: "Password",
+                                  isPassword: true,
+                                  validator: FormBuilderValidators.compose([
+                                    FormBuilderValidators.required(),
+                                    FormBuilderValidators.password()
+                                  ]),
+                                ),
+                                const SizedBox(height: 16),
+                                TextInput(
+                                  name: "confirmPassword",
+                                  label: "Confirm Password",
+                                  isPassword: true,
+                                  errorText: controller.accountFormKey
+                                      .currentState?.errors['confirmPassword'],
+                                  validator: FormBuilderValidators.compose(
+                                    [
+                                      FormBuilderValidators.required(),
+                                      FormBuilderValidators.equal(
+                                        controller.accountFormKey.currentState
+                                                ?.fields['password']?.value ??
+                                            "",
+                                        errorText:
+                                            "Confirm password is not equal to password",
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                const SizedBox(height: 20),
+                                Button(
+                                  onPressed: () {
+                                    var value = _controller
+                                            .passwordFormKey
+                                            .currentState
+                                            ?.instantValue['password'] ??
+                                        "";
+
+                                    _controller.updatePassword(
+                                      value,
+                                    );
+                                  },
+                                  text: "Update password",
+                                  isLoading: controller.isLoadingPassword.value,
+                                )
+                              ]
+                            : [
+                                const SizedBox(height: 6),
+                                TextInput(
+                                  key: const Key("currentPassword"),
+                                  name: "currentPassword",
+                                  label: "Current password",
+                                  validator: FormBuilderValidators.compose([
+                                    FormBuilderValidators.required(),
+                                  ]),
+                                  isPassword: true,
+                                ),
+                                const SizedBox(height: 20),
+                                Button(
+                                  onPressed: () {
+                                    var isValid = _controller
+                                            .passwordFormKey.currentState
+                                            ?.validate() ??
+                                        false;
+                                    var value = _controller
+                                            .passwordFormKey
+                                            .currentState
+                                            ?.instantValue['currentPassword'] ??
+                                        "";
+
+                                    if (isValid) {
+                                      _controller.checkPassword(
+                                        value,
+                                      );
+                                    }
+                                  },
+                                  text: "Check password",
+                                  isLoading: controller.isLoadingPassword.value,
+                                ),
+                              ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
